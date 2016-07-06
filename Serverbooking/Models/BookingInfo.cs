e015e33fe0b3a88add2fd44f3256b76dfd065ab7
vsantos <victoriasantos101@gmail.com>
2016-07-06 16:39:02 -0300

    added class BookingInfo

---
+++ Serverbooking/Models/BookingInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Serverbooking.Models
+{
+    public class BookingInfo
+    {
+        public int BookingID { get; set; }
+        public int ServerID { get; set; }
+        public string UserID { get; set; }
+        public DateTime CheckInTime { get; set; }
+        public DateTime? CheckOutTime { get; set;  } 
+    }
+}
