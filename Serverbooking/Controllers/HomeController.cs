42dde4b02671763528abca93c8c5adfb7bc77060
vsantos <victoriasantos101@gmail.com>
2016-07-21 14:32:08 -0300

    attempting to connect to database

--- Serverbooking/Controllers/HomeController.cs
+++ Serverbooking/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Serverbooking.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -40,6 +41,10 @@ namespace Serverbooking.Controllers
         {
             return Content(@"{ ""records"":[ {""Name"":""Alfreds Futterkiste"",""City"":""Berlin"",""Country"":""Germany""}, {""Name"":""Ana Trujillo Emparedados y helados"",""City"":""México D.F."",""Country"":""Mexico""}, {""Name"":""Antonio Moreno Taquería"",""City"":""México D.F."",""Country"":""Mexico""}, {""Name"":""Around the Horn"",""City"":""London"",""Country"":""UK""}, {""Name"":""B's Beverages"",""City"":""London"",""Country"":""UK""}, {""Name"":""Berglunds snabbköp"",""City"":""Luleå"",""Country"":""Sweden""}, {""Name"":""Blauer See Delikatessen"",""City"":""Mannheim"",""Country"":""Germany""}, {""Name"":""Blondel père et fils"",""City"":""Strasbourg"",""Country"":""France""}, {""Name"":""Bólido Comidas preparadas"",""City"":""Madrid"",""Country"":""Spain""}, {""Name"":""Bon app'"",""City"":""Marseille"",""Country"":""France""}, {""Name"":""Bottom - Dollar Marketse"",""City"":""Tsawassen"",""Country"":""Canada""}, {""Name"":""Cactus Comidas para llevar"",""City"":""Buenos Aires"",""Country"":""Argentina""}, {""Name"":""Centro comercial Moctezuma"",""City"":""México D.F."",""Country"":""Mexico""}, {""Name"":""Chop - suey Chinese"",""City"":""Bern"",""Country"":""Switzerland""}, {""Name"":""Comércio Mineiro"",""City"":""São Paulo"",""Country"":""Brazil""} ] }");
         }
-        
+        public ActionResult Data()
+        {
+            var entities = new ServerInfoEntities();
+            return View(entities.InfoServers.ToList());
+        }
      }
   }
