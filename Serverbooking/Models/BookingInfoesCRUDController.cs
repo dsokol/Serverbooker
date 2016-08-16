304bd12fe1cca325a99605edf33f43fc3350c7af
vsantos <victoriasantos101@gmail.com>
2016-08-16 17:04:15 -0300

    Finally got CRUD to work

---
+++ Serverbooking/Models/BookingInfoesCRUDController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Serverbooking.Models
+{
+    public class BookingInfoesCRUDController : Controller
+    {
+        private ServerInfoEntities db = new ServerInfoEntities();
+
+        // GET: BookingInfoesCRUD
+        public async Task<ActionResult> BookingData()
+        {
+            var bookingInfo = db.BookingInfo.Include(b => b.InfoServer);
+            return View(await bookingInfo.ToListAsync());
+        }
+
+        // GET: BookingInfoesCRUD/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BookingInfo bookingInfo = await db.BookingInfo.FindAsync(id);
+            if (bookingInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bookingInfo);
+        }
+
+        // GET: BookingInfoesCRUD/Create
+        public ActionResult Create()
+        {
+            ViewBag.ServerID = new SelectList(db.InfoServers, "ServerID", "Status");
+            return View();
+        }
+
+        // POST: BookingInfoesCRUD/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "BookingID,ServerID,UserID,CheckInTime,CheckOutTime")] BookingInfo bookingInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.BookingInfo.Add(bookingInfo);
+                await db.SaveChangesAsync();
+                return RedirectToAction("BookingData");
+            }
+
+            ViewBag.ServerID = new SelectList(db.InfoServers, "ServerID", "Status", bookingInfo.ServerID);
+            return View(bookingInfo);
+        }
+
+        // GET: BookingInfoesCRUD/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BookingInfo bookingInfo = await db.BookingInfo.FindAsync(id);
+            if (bookingInfo == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ServerID = new SelectList(db.InfoServers, "ServerID", "Status", bookingInfo.ServerID);
+            return View(bookingInfo);
+        }
+
+        // POST: BookingInfoesCRUD/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "BookingID,ServerID,UserID,CheckInTime,CheckOutTime")] BookingInfo bookingInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(bookingInfo).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("BookingData");
+            }
+            ViewBag.ServerID = new SelectList(db.InfoServers, "ServerID", "Status", bookingInfo.ServerID);
+            return View(bookingInfo);
+        }
+
+        // GET: BookingInfoesCRUD/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BookingInfo bookingInfo = await db.BookingInfo.FindAsync(id);
+            if (bookingInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bookingInfo);
+        }
+
+        // POST: BookingInfoesCRUD/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            BookingInfo bookingInfo = await db.BookingInfo.FindAsync(id);
+            db.BookingInfo.Remove(bookingInfo);
+            await db.SaveChangesAsync();
+            return RedirectToAction("BookingData");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Serverbooking/Models/ServerInfoCRUDController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Serverbooking.Models
+{
+    public class ServerInfoCRUDController : Controller
+    {
+        private ServerInfoEntities db = new ServerInfoEntities();
+
+        // GET: CRUD
+        public async Task<ActionResult> ServerData()
+        {
+            return View(await db.InfoServers.ToListAsync());
+        }
+
+        // GET: CRUD/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            InfoServer infoServer = await db.InfoServers.FindAsync(id);
+            if (infoServer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(infoServer);
+        }
+
+        // GET: CRUD/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CRUD/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "ServerID,Status,ServerName,Environment,ActiveBookingID")] InfoServer infoServer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.InfoServers.Add(infoServer);
+                await db.SaveChangesAsync();
+                return RedirectToAction("ServerData");
+            }
+
+            return View(infoServer);
+        }
+
+        // GET: CRUD/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            InfoServer infoServer = await db.InfoServers.FindAsync(id);
+            if (infoServer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(infoServer);
+        }
+
+        // POST: CRUD/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "ServerID,Status,ServerName,Environment,ActiveBookingID")] InfoServer infoServer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(infoServer).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("ServerData");
+            }
+            return View(infoServer);
+        }
+
+        // GET: CRUD/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            InfoServer infoServer = await db.InfoServers.FindAsync(id);
+            if (infoServer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(infoServer);
+        }
+
+        // POST: CRUD/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            InfoServer infoServer = await db.InfoServers.FindAsync(id);
+            db.InfoServers.Remove(infoServer);
+            await db.SaveChangesAsync();
+            return RedirectToAction("ServerData");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
