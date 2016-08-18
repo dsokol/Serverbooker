a533329ef9cd1ba9bc197c5eee41494329fdccfc
vsantos <victoriasantos101@gmail.com>
2016-08-18 14:58:35 -0300

    trying to allow login

--- Serverbooking/Controllers/HomeController.cs
+++ Serverbooking/Controllers/HomeController.cs
@@ -32,6 +32,37 @@ namespace Serverbooking.Controllers
         }
 
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(TestUser u)
+        {
+     
+            if (ModelState.IsValid) 
+            {
+                using (ServerInfoEntities db = new ServerInfoEntities())
+                {
+                    var v = db.TestUsers.Where(a => a.Username.Equals(u.Username) && a.Password.Equals(u.Password)).FirstOrDefault();
+                    if (v != null)
+                    {
+                        Session["LogedUserID"] = v.UserID.ToString();
+                        Session["LogedUserFullname"] = v.FullName.ToString();
+                        return RedirectToAction("AfterLogin");
+                    }
+                }
+            }
+            return View(u);
+        }
+        public ActionResult AfterLogin()
+        {
+            if (Session["LogedUserID"] != null)
+            {
+                return View("Index");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
     }
 }
 
