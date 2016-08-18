d09567e8a09a35fe6cf055b15c733b73311a3501
vsantos <victoriasantos101@gmail.com>
2016-08-18 11:04:52 -0300

    made issue+fixed issues, also trying to allow login

--- Serverbooking/Controllers/HomeController.cs
+++ Serverbooking/Controllers/HomeController.cs
@@ -18,31 +18,19 @@ namespace Serverbooking.Controllers
 
         public ActionResult Index()
         {
-            ViewBag.Message = "Index";
             return View();
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Contact page.";
-
             return View();
         }
-        //public ActionResult ServerInfo()
-        //{
-        //    var serverinfo = new List<object>();
-        //    serverinfo.Add(new { ServerID = "78078", Status = "Busy", ServerName = "TMR234", Environment = "RMS", ActiveBookingID = "Act234" });
-        //    serverinfo.Add(new { ServerID = "24090", Status = "Free", ServerName = "TMR942", Environment = "TSM", ActiveBookingID = "Act942" });
-        //    serverinfo.Add(new { ServerID = "25069", Status = "Busy", ServerName = "TMR165", Environment = "TRM", ActiveBookingID = "Act165" });
-        //    return Json(serverinfo, JsonRequestBehavior.AllowGet);
-        //}
+
 
     }
 }
