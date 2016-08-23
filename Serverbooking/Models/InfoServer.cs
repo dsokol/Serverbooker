1261435c7d4e59f0a528c79b5a4eefdb82357747
vsantos <victoriasantos101@gmail.com>
2016-08-23 09:53:18 -0300

    fixed issue with Username in serverinfo

--- Serverbooking/Models/InfoServer.cs
+++ Serverbooking/Models/InfoServer.cs
@@ -25,7 +25,7 @@ namespace Serverbooking.Models
         public string ServerName { get; set; }
         public string Environment { get; set; }
         public Nullable<int> ActiveBookingID { get; set; }
-        public string UserID { get; set;  }
+        public string UserID { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<BookingInfo> BookingInfo { get; set; }
